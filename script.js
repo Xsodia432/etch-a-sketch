65b567891aa04aebfe1434d3c6e40d77efe8a6e2
clarence <reyannpahinado@gmail.com>
2025-02-09 09:25:02 +0800

    Added the ability to change colors while hovering

---
+++ script.js
@@ -0,0 +1,49 @@
+const sketchDiv = document.getElementById("container");
+const addSquaresButton = document.getElementById("add-squares");
+
+const drawPad = (squares) => {
+  const padContainer = document.createElement("div");
+
+  padContainer.classList = "sketch-pad-container";
+  sketchDiv.appendChild(padContainer);
+  for (let row = 0; row < squares; row++) {
+    for (let col = 0; col < squares; col++) {
+      let opacity = 10;
+
+      const rowDiv = document.createElement("div");
+      rowDiv.classList = "col";
+      rowDiv.setAttribute(
+        "style",
+        `flex: 0 0 calc(100% / ${squares});
+    background-color: white;`
+      );
+      rowDiv.addEventListener("mouseenter", (e) => {
+        const randColor = [
+          Math.floor(Math.random() * 255),
+          Math.floor(Math.random() * 255),
+          Math.floor(Math.random() * 255),
+        ];
+        rowDiv.style.backgroundColor = `rgba(${randColor[0]},${randColor[1]},${randColor[2]},${opacity}%)`;
+        opacity += 10;
+      });
+      rowDiv.addEventListener("mouseup", (e) => {
+        clearInterval(isMouseHeld);
+        console.log("mouse not being held");
+      });
+      padContainer.append(rowDiv);
+    }
+  }
+};
+
+addSquaresButton.addEventListener("click", () => {
+  const squares = parseInt(prompt("Add squares maximum is 100"));
+  if (squares > 100 || !squares) return;
+  else {
+    document.getElementById(
+      "num-squares"
+    ).textContent = ` Number of squares: ${squares}`;
+    document.querySelector(".sketch-pad-container").remove();
+    drawPad(squares);
+  }
+});
+drawPad(16);
